2e1143cb05bfcaa9ff32e74a721f9bd7454064dd
sumo47 <sumokhan76342@gmail.com>
2024-04-28 21:54:36 +0530

    Navbar part 3

---
+++ src/components/Navbar/SearchBar/SearchBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './SearchBar.css'
+import { FaSearch } from 'react-icons/fa'
+
+function SearchBar() {
+    return (
+        <>
+            <div className="SearchBar_Container">
+                <div className="SearchBar_Container2">
+                    <div className="search_div">
+                        <input type="text" className='iBox_SearchBar' />
+                        <FaSearch className='searchIcon_SearchBar'/>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default SearchBar
