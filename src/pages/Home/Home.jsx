87ccdf453ac238bbfe17b7ce208d39adc4ec9f28
sumo47 <sumokhan76342@gmail.com>
2024-05-09 01:19:53 +0530

    HomePage Category list

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
             video_src: vid,
             Chanel: "62bafe6752cea35a6c30685f",
             title: "video 1",
-            Uploader:"sumit",
+            Uploader: "sumit",
             description: "description of video 1"
         },
         {
@@ -19,7 +19,7 @@ function Home() {
             video_src: vid,
             Chanel: "cdd",
             title: "video 2",
-            Uploader:"sumit",
+            Uploader: "sumit",
 
             description: "description of video 2"
         },
@@ -28,7 +28,7 @@ function Home() {
             video_src: vid,
             Chanel: "add",
             title: "video 3",
-            Uploader:"sumit",
+            Uploader: "sumit",
 
             description: "description of video 3"
         },
@@ -37,7 +37,7 @@ function Home() {
             video_src: vid,
             Chanel: "add",
             title: "video 4",
-            Uploader:"sumit",
+            Uploader: "sumit",
 
             description: "description of video 4"
         },
@@ -46,16 +46,27 @@ function Home() {
             video_src: vid,
             Chanel: "add",
             title: "video 5",
-            Uploader:"sumit",
+            Uploader: "sumit",
 
             description: "description of video 5"
         },
+    ]
+
+    const NavList = [
+        "All", "Music", "Movies", "comedy", "python", "computer science","All", "Music", "Movies", "comedy", "python", "computer science"
     ]
 
     return (
         <div className='container_Pages_App'>
             <LeftSideBar />
             <div className="container2_pages_App">
+                <div className="navigation_Home">
+                    {NavList.map(m => {
+                        return (
+                            <p key={m} className='btn_nav_home'>{m}</p>
+                        )
+                    })}
+                </div>
                 <ShowVideoGrid vids={vids} />
             </div>
 
